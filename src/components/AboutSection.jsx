00001baf96a8effec2b8768254e85e9e82cf21ba
Priyansh Raj Gupta <135385611+Priyanshraj26@users.noreply.github.com>
2025-10-31 01:19:56 +0530

    Resume Updation

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1ziDQIvs-dTyLuRilkcsGUmkJu0W13F-P/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1ziDQIvs-dTyLuRilkcsGUmkJu0W13F-P/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
